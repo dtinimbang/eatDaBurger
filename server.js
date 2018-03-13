7d95ea2fe78e6cd4e281b49865d7e6c905a6af71
don <don.tinimbang@gmail.com>
2018-03-13 17:58:10 -0500

    forgot to save

--- server.js
+++ server.js
@@ -20,7 +20,8 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
+// Import routes and 
+give the server access to them.
 var routes = require("./controllers/burgersController.js");
 
 app.use(routes);
