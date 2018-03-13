e5583a65ab4ef4a1a211687c41abf9b0d60cb71e
don <don.tinimbang@gmail.com>
2018-03-13 17:56:23 -0500

    port was not working, fixed js.server

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -26,14 +26,14 @@ var burger = require("../models/burger.js");
 router.get("/", function(req, res){
     burger.all(function(data){
         var hbsObject = {
-            cats: data
+            burgers: data
         };
         console.log(hbsObject);
         res.render("index", hbsObject);
     });
 });
 
-router.post("/api/burgers", function(req, res){
+router.post("/burgers", function(req, res){
     burger.create([
         "name"
     ],[
@@ -43,7 +43,7 @@ router.post("/api/burgers", function(req, res){
     });
 });
     
-router.put("/api/burgers/:id", function(req, res) {
+router.put("/burgers/:id", function(req, res) {
     var condition = "id = " + req.params.id;
 
     console.log("condition", condition);
@@ -60,18 +60,7 @@ router.put("/api/burgers/:id", function(req, res) {
       });
     });
     
-    router.delete("/api/burgers/:id", function(req, res) {
-      var condition = "id = " + req.params.id;
-    
-      burger.delete(condition, function(result) {
-        if (result.affectedRows == 0) {
-          // If no rows were changed, then the ID must not exist, so 404
-          return res.status(404).end();
-        } else {
-          res.status(200).end();
-        }
-      });
-    });
+
 
 // Export routes for server.js to use.
 module.exports = router;
