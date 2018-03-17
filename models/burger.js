98f93f0b7c68e46c1eeaeb409de48e900cbff993
don <don.tinimbang@gmail.com>
2018-03-17 00:27:02 -0500

    made changes to reflect orm.js

--- models/burger.js
+++ models/burger.js
@@ -13,18 +13,18 @@
 
 var orm = ("../config/orm.js");
 var burger = {
-    all: function (cb) {
+    selectAll: function (cb) {
         orm.all("burgers", function (res) {
             cb(res);
         });
     },
     // The variables cols and vals are arrays.
-    create: function (cols, vals, cb) {
+    insertOne: function (cols, vals, cb) {
         orm.create("burgers", cols, vals, function (res) {
             cb(res);
         });
     },
-    update: function (objColVals, condition, cb) {
+    createOne: function (objColVals, condition, cb) {
         orm.update("burgers", objColVals, condition, function (res) {
             cb(res);
         });
